2bf030cd9ce382ad1221688fd415cdc435e7d1e7
Kicer Jiao <joykicer@gmail.com>
2014-04-26 14:02:23 +0800

    [INIT] make standlone to test basic build success

--- workspace/src/main.c
+++ workspace/src/main.c
@@ -20,8 +20,7 @@
   */ 
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f10x.h"
-#include "stm32_eval.h"
+#include <stm32f10x.h>
 
 /** @addtogroup STM32F10x_StdPeriph_Examples
   * @{
@@ -123,33 +122,12 @@ int main(void)
 }
 
 #ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) { 
+	/* User can add his own implementation to report the file name and line number,
+	 * ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
+	/* Infinite loop */
+	while (1){
+	}
 }
-
 #endif
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
